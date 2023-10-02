f357d8706d95c42f653f97486189b2f65704dbb1
2WebBuilders <contact.webbuilderss@gmail.com>
2023-10-02 20:04:01 -0300

    Rotina de Pagamento PIX

--- src/app/pages/carteira/carteira.component.ts
+++ src/app/pages/carteira/carteira.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ClipboardModule } from 'ngx-clipboard';
 
 @Component({
   selector: 'app-carteira',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./carteira.component.scss']
 })
 export class CarteiraComponent {
+  minutes: number = 10;
+  seconds: number = 0;
+  isCounting: boolean = false;
+  
 
+  ngOnInit(): void {
+  }
+
+  startCountdown() {
+    if (!this.isCounting) {
+      this.isCounting = true;
+      const timerInterval = setInterval(() => {
+        if (this.seconds > 0) {
+          this.seconds--;
+        } else if (this.minutes > 0) {
+          this.minutes--;
+          this.seconds = 59;
+        } else {
+          clearInterval(timerInterval);
+          this.isCounting = false;
+        }
+      }, 1000);
+    }
+  }
 }
